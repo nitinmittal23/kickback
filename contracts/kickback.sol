bffe5d0539e26f2037575f0695938e71d3fd63aa
anjalijoshi7 <anjalijoshi342@gmail.com>
2020-04-27 17:08:43 +0530

    added contract

---
+++ contracts/kickback.sol
@@ -0,0 +1,67 @@
+pragma solidity 0.5.16;
+
+contract kickback {
+    
+    struct Person {
+        address unique;
+        string name;
+        uint total_count;
+        uint total_failure;
+        uint start;
+        uint end;
+
+    }
+    
+    mapping (address => Person) Allperson;
+    address[] public personIds;
+    address[] public winningIds; 
+    
+
+    function getAlladdress() public view returns(address[] memory){
+        return personIds;
+    }
+
+    function getaccount(address _address) public view returns(address, string memory, uint, uint, uint, uint){
+        return (Allperson[_address].unique, Allperson[_address].name, Allperson[_address].total_count, Allperson[_address].total_failure, Allperson[_address].start, Allperson[_address].end);
+    }
+
+    function updateData(address _address, string memory _name) public  {
+        for(uint i =0; i < personIds.length; i++) {
+            if( Allperson[personIds[i]].total_count > 0) {
+                if(now > Allperson[personIds[i]].end) {
+                    Allperson[_address].total_failure = Allperson[_address].total_failure + 1;
+                    Allperson[_address].start = Allperson[_address].start + 86400;
+                    Allperson[_address].end = Allperson[_address].end + 86400;
+                }
+            }
+        }
+
+        if(Allperson[_address].total_count == 0) {
+            Allperson[_address].unique = _address;
+            Allperson[_address].name = _name;
+            Allperson[_address].total_count = 1;
+            Allperson[_address].total_failure = 0;
+            Allperson[_address].start = now + 86400;
+            Allperson[_address].end = now + 172800;
+        }
+        else {
+            Allperson[_address].start = Allperson[_address].start + 86400;
+            Allperson[_address].end = Allperson[_address].end + 86400;
+            Allperson[_address].total_count = Allperson[_address].total_count + 1;
+            
+
+        }
+           winningIds.length = 0;     
+        for(uint i =0; i < personIds.length; i++) {
+            if(Allperson[personIds[i]].total_count >= 27) {
+                winningIds.push(personIds[i]);
+                 }
+        }
+        
+    }
+
+    function winners() public view  returns(address[] memory ) { 
+     
+         return winningIds;
+    }
+}
